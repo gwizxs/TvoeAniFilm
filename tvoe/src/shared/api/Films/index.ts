b4956cc7dfc566bfb69a7124e479c28b960bd426
gwizxs <gwizxs@gmail.com>
2024-10-08 15:10:27 +0300

    add: search films

--- tvoe/src/shared/api/Films/index.ts
+++ tvoe/src/shared/api/Films/index.ts
@@ -4,15 +4,24 @@ import { Movie } from "./types";
 
 
 
-export const getFilms = async ({page, limit}: {page: number, limit: number}): Promise<Movie[]> => {
+export const getFilms = async ({ page, limit }: { page: number, limit: number }): Promise<Movie[]> => {
     const axiosInstance = createInstanceV2;
-        const response = (await axiosInstance.get(FILMS_URL.films(), {
-            params: {
-                page,
-                limit, 
-                query: 'top250',
-                notNullFields: ['poster.url']
-            }
-        }))
-        return response.data; 
+    const response = (await axiosInstance.get(FILMS_URL.films(), {
+        params: {
+            page,
+            limit,
+            notNullFields: 'poster.url'
+        }
+    }))
+    return response.data;
 };
+
+
+export const SearchFilms = async (searchTerm: string) => {
+    const response = await createInstanceV2.get(FILMS_URL.search_films_name(), {
+        params: {
+            query: searchTerm,
+        },
+    })
+    return response;
+}

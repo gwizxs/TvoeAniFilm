7e513d22c46d72d2d9e643df8d28cc4703ef9b91
gwizxs <gwizxs@gmail.com>
2024-10-29 17:42:44 +0300

    add animation in items header

--- tvoe/src/widgets/header/components/navbar/index.tsx
+++ tvoe/src/widgets/header/components/navbar/index.tsx
@@ -7,6 +7,7 @@ import { menu } from '@/shared/constants/menu';
 import { MobileNavBar } from '../navbar-mobile'
 import s from './navbar.module.scss';
 import { Button } from 'antd';
+import { motion } from 'framer-motion';
 
 export const NavBar = () => {
    const isMobile = useMobile(640);
@@ -14,25 +15,32 @@ export const NavBar = () => {
    if (isMobile) return <MobileNavBar />
 
    return (
-      <nav className={s.nav}>
-         <ul className={clsx('list-reset', s.list)}>
-            {menu.map((item) => {
-               return (
-                  <li className={s.item} key={item.text}>
-                     <Link
-                        className={clsx(s.link, s.isCurrent)}
-                        href={item.url}
-                     >
-                        <Button type='text'>
-                           <span>{item.icon}</span>
-                           <span>{item.text}</span>
-                        </Button>
-                     </Link>
-                  </li>
-               );
-            })}
-         </ul>
-      </nav>
+
+<motion.nav
+  className={s.nav}
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 0.5 }}
+>
+  <ul className={clsx('list-reset', s.list)}>
+    {menu.map((item) => (
+      <motion.li
+        className={s.item}
+        key={item.text}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.9, delay: 0.1 }}
+      >
+        <Link className={clsx(s.link, s.isCurrent)} href={item.url}>
+          <Button type='text'>
+            <span>{item.icon}</span>
+            <span>{item.text}</span>
+          </Button>
+        </Link>
+      </motion.li>
+    ))}
+  </ul>
+</motion.nav>
    );
 };
 
